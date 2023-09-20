45222eb8581895056bc5a9f5a7adbb2cbe88b91b
prographicsten <prographicsten@gmail.com>
2023-09-20 18:06:27 +0600

    done!

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,20 @@
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Home = () => {
+    const navigation = useNavigation();
+    let location = useLocation();
+    console.log(location);
+
     return (
         <div className="text-center mt-5">
             <Header></Header>
-
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ? <p>Loding...</p> :
+                <Outlet></Outlet>
+            }
 
             <Footer></Footer>
         </div>
